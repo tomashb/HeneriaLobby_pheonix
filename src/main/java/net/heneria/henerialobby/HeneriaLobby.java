6bb4d32cca2e12ee3489d3cbadf6e3fa87a2ab18
tomashb <62190147+tomashb@users.noreply.github.com>
2025-09-04 22:51:40 +0200

    feat: introduce lobby admin reload and custom commands

--- src/main/java/net/heneria/henerialobby/HeneriaLobby.java
+++ src/main/java/net/heneria/henerialobby/HeneriaLobby.java
@@ -2,6 +2,8 @@ package net.heneria.henerialobby;
 
 import com.google.common.io.ByteStreams;
 import me.clip.placeholderapi.PlaceholderAPI;
+import net.heneria.henerialobby.command.CustomCommand;
+import net.heneria.henerialobby.command.LobbyAdminCommand;
 import net.heneria.henerialobby.command.LobbyCommand;
 import net.heneria.henerialobby.command.SetLobbyCommand;
 import net.heneria.henerialobby.command.ServersCommand;
@@ -18,12 +20,18 @@ import net.heneria.henerialobby.selector.ServerSelector;
 import net.heneria.henerialobby.spawn.SpawnManager;
 import net.heneria.henerialobby.visibility.VisibilityManager;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class HeneriaLobby extends JavaPlugin {
 
@@ -42,6 +50,7 @@ public class HeneriaLobby extends JavaPlugin {
     private TablistManager tablistManager;
     private VisibilityManager visibilityManager;
     private java.util.Set<String> lobbyWorlds;
+    private final java.util.Map<String, Command> customCommands = new java.util.HashMap<>();
 
     @Override
     public void onEnable() {
@@ -51,6 +60,7 @@ public class HeneriaLobby extends JavaPlugin {
         saveResource("messages.yml", false);
         saveResource("server-selector.yml", false);
         saveResource("scoreboard.yml", false);
+        saveResource("commands.yml", false);
         messages = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "messages.yml"));
         scoreboardConfig = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "scoreboard.yml"));
         spawnManager = new SpawnManager(this);
@@ -68,6 +78,7 @@ public class HeneriaLobby extends JavaPlugin {
         getCommand("lobby").setExecutor(new LobbyCommand(this, spawnManager));
         getCommand("setlobby").setExecutor(new SetLobbyCommand(this, spawnManager));
         getCommand("servers").setExecutor(new ServersCommand(serverSelector));
+        getCommand("lobbyadmin").setExecutor(new LobbyAdminCommand(this));
         Bukkit.getPluginManager().registerEvents(new SpawnListener(this, spawnManager), this);
         Bukkit.getPluginManager().registerEvents(new SelectorListener(this, serverSelector), this);
         if (getConfig().getBoolean("protection.enabled", true)) {
@@ -97,10 +108,13 @@ public class HeneriaLobby extends JavaPlugin {
             long interval = getConfig().getLong("tablist.update-interval", 40L);
             getServer().getScheduler().runTaskTimer(this, () -> tablistManager.updateAll(), 0L, interval);
         }
+
+        loadCustomCommands();
     }
 
     @Override
       public void onDisable() {
+          unregisterCustomCommands();
           this.getServer().getMessenger().unregisterOutgoingPluginChannel(this, VELOCITY_CONNECT);
       }
 
@@ -121,28 +135,141 @@ public class HeneriaLobby extends JavaPlugin {
           return world != null && lobbyWorlds.contains(world.getName());
       }
 
-      public void updateDisplays(Player player) {
-          if (scoreboardManager != null || tablistManager != null) {
-              if (isLobbyWorld(player.getWorld())) {
-                  if (scoreboardManager != null) {
-                      scoreboardManager.update(player);
-                  }
-                  if (tablistManager != null) {
-                      tablistManager.update(player);
-                  }
-              } else {
-                  if (scoreboardManager != null) {
-                      var manager = Bukkit.getScoreboardManager();
-                      if (manager != null) {
-                          player.setScoreboard(manager.getNewScoreboard());
-                      }
-                  }
-                  if (tablistManager != null) {
-                      player.setPlayerListHeaderFooter("", "");
-                  }
-              }
-          }
-      }
+    public void updateDisplays(Player player) {
+        if (scoreboardManager != null || tablistManager != null) {
+            if (isLobbyWorld(player.getWorld())) {
+                if (scoreboardManager != null) {
+                    scoreboardManager.update(player);
+                }
+                if (tablistManager != null) {
+                    tablistManager.update(player);
+                }
+            } else {
+                if (scoreboardManager != null) {
+                    var manager = Bukkit.getScoreboardManager();
+                    if (manager != null) {
+                        player.setScoreboard(manager.getNewScoreboard());
+                    }
+                }
+                if (tablistManager != null) {
+                    player.setPlayerListHeaderFooter("", "");
+                }
+            }
+        }
+    }
+
+    public void reloadAll() {
+        reloadConfig();
+        messages = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "messages.yml"));
+        scoreboardConfig = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "scoreboard.yml"));
+        spawnManager = new SpawnManager(this);
+        serverSelector = new ServerSelector(this);
+        lobbyWorlds = new java.util.HashSet<>(getConfig().getStringList("lobby-worlds"));
+
+        getCommand("lobby").setExecutor(new LobbyCommand(this, spawnManager));
+        getCommand("setlobby").setExecutor(new SetLobbyCommand(this, spawnManager));
+        getCommand("servers").setExecutor(new ServersCommand(serverSelector));
+
+        org.bukkit.Bukkit.getScheduler().cancelTasks(this);
+        HandlerList.unregisterAll(this);
+
+        if (getConfig().getBoolean("scoreboard.enabled", true)) {
+            scoreboardManager = new ScoreboardManager(this, scoreboardConfig);
+            long interval = getConfig().getLong("scoreboard.update-interval", 40L);
+            getServer().getScheduler().runTaskTimer(this, () -> scoreboardManager.updateAll(), 0L, interval);
+        } else {
+            scoreboardManager = null;
+        }
+
+        if (getConfig().getBoolean("tablist.enabled", true)) {
+            tablistManager = new TablistManager(this, scoreboardConfig);
+            long interval = getConfig().getLong("tablist.update-interval", 40L);
+            getServer().getScheduler().runTaskTimer(this, () -> tablistManager.updateAll(), 0L, interval);
+        } else {
+            tablistManager = null;
+        }
+
+        visibilityManager = null;
+        if (getConfig().getBoolean("player-experience.player-visibility.enabled", true)) {
+            visibilityManager = new VisibilityManager(this);
+            Bukkit.getPluginManager().registerEvents(new VisibilityListener(this, visibilityManager), this);
+        }
+
+        Bukkit.getPluginManager().registerEvents(new SpawnListener(this, spawnManager), this);
+        Bukkit.getPluginManager().registerEvents(new SelectorListener(this, serverSelector), this);
+        if (getConfig().getBoolean("protection.enabled", true)) {
+            Bukkit.getPluginManager().registerEvents(new ProtectionListener(this), this);
+        }
+        Bukkit.getPluginManager().registerEvents(new DisplayListener(this), this);
+        if (getConfig().getBoolean("player-experience.launchpads.enabled", true)) {
+            Bukkit.getPluginManager().registerEvents(new LaunchpadListener(this), this);
+        }
+        if (getConfig().getBoolean("player-experience.join-leave-messages.enabled", true)) {
+            Bukkit.getPluginManager().registerEvents(new JoinLeaveListener(this), this);
+        }
+
+        loadCustomCommands();
+    }
+
+    private CommandMap getCommandMap() {
+        try {
+            Method method = Bukkit.getServer().getClass().getMethod("getCommandMap");
+            return (CommandMap) method.invoke(Bukkit.getServer());
+        } catch (Exception e) {
+            getLogger().warning("Failed to get command map: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private void unregisterCustomCommands() {
+        CommandMap map = getCommandMap();
+        if (map == null) {
+            return;
+        }
+        try {
+            Field f = map.getClass().getDeclaredField("knownCommands");
+            f.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            java.util.Map<String, Command> known = (java.util.Map<String, Command>) f.get(map);
+            for (Command cmd : customCommands.values()) {
+                cmd.unregister(map);
+                known.remove(cmd.getName());
+                for (String alias : cmd.getAliases()) {
+                    known.remove(alias);
+                }
+            }
+        } catch (Exception e) {
+            getLogger().warning("Failed to unregister commands: " + e.getMessage());
+        }
+        customCommands.clear();
+    }
+
+    private void loadCustomCommands() {
+        unregisterCustomCommands();
+        File file = new File(getDataFolder(), "commands.yml");
+        FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+        CommandMap map = getCommandMap();
+        if (map == null) {
+            return;
+        }
+        for (String key : cfg.getKeys(false)) {
+            ConfigurationSection sec = cfg.getConfigurationSection(key);
+            if (sec == null) {
+                continue;
+            }
+            java.util.List<String> aliases = sec.getStringList("aliases");
+            Object msgObj = sec.get("message");
+            java.util.List<String> messages;
+            if (msgObj instanceof java.util.List) {
+                messages = sec.getStringList("message");
+            } else {
+                messages = java.util.List.of(sec.getString("message", ""));
+            }
+            CustomCommand cmd = new CustomCommand(key, aliases, messages, this);
+            map.register(getDescription().getName().toLowerCase(), cmd);
+            customCommands.put(key, cmd);
+        }
+    }
 
     public String getMessage(String key) {
         String message = messages.getString(key, key);

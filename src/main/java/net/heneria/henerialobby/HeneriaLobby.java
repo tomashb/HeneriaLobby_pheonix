5a4fb51507899ef9043aecb6bbb4674583b89706
tomashb <62190147+tomashb@users.noreply.github.com>
2025-09-06 15:11:20 +0200

    fix: add missing Plugin import

--- src/main/java/net/heneria/henerialobby/HeneriaLobby.java
+++ src/main/java/net/heneria/henerialobby/HeneriaLobby.java
@@ -37,6 +37,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
